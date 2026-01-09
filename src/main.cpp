891d1ceea718daaff30496eeeab3fdfaddf334bd
jzy511 <jzy511@qq.com>
2026-01-09 20:02:53 +0800

    pwm控制

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,15 @@
 #include <Arduino.h>
 
-void setup()
-{
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(D0, OUTPUT);
-  pinMode(D1, OUTPUT);
-  pinMode(D2, OUTPUT);
-  pinMode(D3, OUTPUT);
-  pinMode(D4, OUTPUT);
-  pinMode(D5, OUTPUT);
-  pinMode(D6, OUTPUT);
-  digitalWrite(D0, HIGH);
-  digitalWrite(D1, HIGH);
+// pwm test
+
+void setup() {
+    pinMode(D0, OUTPUT);
 }
 
-void loop()
-{
-  if (Serial.available())
-  {
-    String inputString = Serial.readString();
-    Serial.print("I received: ");
-    Serial.println(inputString);
-    // 这块开发板是低电平触发，低电平灯亮
-    if (inputString == "ON")
-    {
-      digitalWrite(LED_BUILTIN, LOW); // 灯亮
+void loop() {
+    // 建立循环，实现D0口PWM输出
+    for (int i = 0; i < 255; i++) {
+        analogWrite(D0, i);
+        delay(500);
     }
-    else if (inputString == "OFF")
-    {
-      digitalWrite(LED_BUILTIN, HIGH); // 灯灭
-    }
-
-    if (inputString == "D0ON")
-    {
-      digitalWrite(D0, HIGH); // D0口高电平
-    }
-    else if (inputString == "D0OFF")
-    {
-      digitalWrite(D0, LOW); // D0口低电平
-    }
-  }
 }
